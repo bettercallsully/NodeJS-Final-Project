30e6d909be1d63a1c4e3395c0a3153b60aaf96bc
Solomon El-kelani <bettercallsully@users.noreply.github.com>
2019-12-12 17:18:45 -0600

    herokudeployment

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('node')
+const app = node()
 const port = 3000
 const app = mongodb()
 
 app.get('/', (req, res) => res.send(''))
 
 //MongoDB Database Config Link
-mongodb+srv://solomonelkelani:campbell1996@cluster0-eg3u3.mongodb.net/test?retryWrites=true&w=majority
+mongodb+srv;//solomonelkelani:campbell1996@cluster0-eg3u3.mongodb.net/test?retryWrites=true&w=majority
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
